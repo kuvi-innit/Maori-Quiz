11504d5a1c76bb944ba3e26dcd38b79e5202b5cc
Kuvam Singh <ac121634@avcol.school.nz>
2023-03-21 14:02:25 +1300

    added if statement so u cant enter letters or other numbers then 1-5 for the levels

--- Program.cs
+++ Program.cs
@@ -9,15 +9,44 @@
             Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello, " + name + "!"); Console.WriteLine("Which level would you like to play?");
-            Console.WriteLine("1 - Beginner");
-            Console.WriteLine("2 - Intermediate");
-            Console.WriteLine("3 - Expert"); int level;
+            Console.WriteLine("1 - Born Yesterday");
+            Console.WriteLine("2 - Beginner");
+            Console.WriteLine("3 - Intermediate");
+            Console.WriteLine("4 - Expert");
+            Console.WriteLine("5 - Albert Einstein");
+            
+            string level;
+            bool levelExists = false;
             do
             {
-                Console.Write("Enter your choice (1-3): ");
-                level = Convert.ToInt32(Console.ReadLine());
-            } while (level < 1 || level > 3); Console.WriteLine($"You have chosen level {level}.");
+                Console.Write("Enter your choice (1-5): ");
+                level = Console.ReadLine();
+
+                if (level == "1")
+                {
+                    levelExists = true;
+                }
+                else if (level == "2")
+                {
+                    levelExists = true;
+                }
+                else if (level == "3")
+                {
+                    levelExists = true;
+                }
+                else if (level == "4")
+                {
+                    levelExists=true;
+                }
+                else if (level == "5")
+                {
+                    levelExists=(true);
+                }
+            } while (levelExists == false);
+            Console.WriteLine($"You have chosen level {level}.");
             Console.WriteLine("Good luck and have fun!");
+      
+        
         }
     }
 }
