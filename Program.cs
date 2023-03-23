6fc17c4d6ddd66fcd50d4f6f8b47f240b4f7eb61
Kuvam Singh <ac121634@avcol.school.nz>
2023-03-24 10:10:32 +1300

    Added questions all the questions onto difficulty one

--- Program.cs
+++ Program.cs
@@ -49,8 +49,91 @@
             Console.Clear();
             Console.WriteLine($"You have chosen level {level}!");
             Console.WriteLine("\nGood luck and have fun!");
-      
-        
+            //end of Level method 
+            int score = 0;
+            string questioninput;
+            bool questioninputExists = false;
+            do
+            {
+                questioninput=Console.ReadLine();
+                if (questioninput =="A")
+            {
+                questioninputExists = true;
+            }
+            else if (questioninput == "B")
+            {
+                questioninputExists = true;
+            }
+            else if (questioninput == "C")
+            {
+                questioninputExists = true;
+            }
+            else if (questioninput == "D")
+            {
+                questioninputExists = true;
+            }
+                Console.WriteLine("Welcome to the Maori Quiz!");
+
+
+
+                Console.WriteLine("Answer the following questions by typing the letter of the correct answer.");
+                Console.WriteLine("1. What is the Maori word for hello?");
+                Console.WriteLine("A. Kia ora");
+                Console.WriteLine("B. Haere mai");
+                Console.WriteLine("C. Ka kite ano");
+                Console.WriteLine("D. Nau mai");
+                string answer1 = Console.ReadLine().ToUpper();
+                if (answer1 == "A") { Console.WriteLine("Correct!"); score++; }
+                else
+                {
+                    Console.WriteLine("Incorrect. The correct answer is A.");
+                }
+                Console.WriteLine("2. What is the meaning of the word 'whanau'?");
+                Console.WriteLine("A. Family");
+                Console.WriteLine("B. Friend");
+                Console.WriteLine("C. Love");
+                Console.WriteLine("D. Life"); string answer2 = Console.ReadLine().ToUpper();
+                if
+                    (answer2 == "A") { Console.WriteLine("Correct!"); score++; }
+                else
+                { Console.WriteLine("Incorrect. The correct answer is A."); }
+                Console.WriteLine("3. What is the Maori name for the North Island of New Zealand?"); Console.WriteLine("A. Te Waipounamu");
+                Console.WriteLine("B. Rakiura");
+                Console.WriteLine("C. Aotearoa");
+                Console.WriteLine("D. Te Ika-a-Maui"); string answer3 = Console.ReadLine().ToUpper(); if
+                    (answer3 == "D") { Console.WriteLine("Correct!"); score++; }
+                else
+                {
+                    Console.WriteLine("Incorrect. The correct answer is D.");
+                }
+                Console.WriteLine("4. What is the Maori name for the haka dance?");
+                Console.WriteLine("A. Waiata");
+                Console.WriteLine("B. Kapa haka");
+                Console.WriteLine("C. Whakatauki");
+                Console.WriteLine("D. Wairua"); string answer4 = Console.ReadLine().ToUpper();
+                if (answer4 == "B") { Console.WriteLine("Correct!"); score++; }
+                else
+                {
+                    Console.WriteLine("Incorrect. The correct answer is B.");
+                }
+                Console.WriteLine("5. What is the Maori word for goodbye?");
+                Console.WriteLine("A. Nau mai"); Console.WriteLine("B. Ka kite ano");
+                Console.WriteLine("C. Haere ra"); Console.WriteLine("D. E noho ra");
+                string answer5 = Console.ReadLine().ToUpper();
+                if (answer5 == "C")
+                {
+                    Console.WriteLine("Correct!"); score++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect. The correct answer is C.");
+                }
+                Console.Clear();
+                Console.WriteLine($"your total score out of 5/{score}");
+
+            } while (questioninputExists == false);
+           
+
         }
     }
 }
