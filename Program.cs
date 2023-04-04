865866b13efdae75be024f2ccfbe07c3b660fc5e
Kuvam Singh <ac121634@avcol.school.nz>
2023-04-04 21:22:40 +1200

    Added easy difficulty questions & answers in arrays

--- Program.cs
+++ Program.cs
@@ -23,6 +23,24 @@
                 difficulty = Console.ReadLine().ToLower();
             }
             Console.Clear();
+           
+            // Easy Difficulty
+            if (difficulty == "1")
+            {
+                string[] easyQuestions = {
+                "1. What is the Maori term for 'New Zealand'?\nA) Aotearoa\nB) Hawaii\nC) Australia\nD) Fiji",
+                "2. What is the traditional Maori greeting?\nA) Aloha\nB) Kia Ora\nC) Hola\nD) Bonjour",
+                "3. What animal is a symbol of New Zealand?\nA) Emu\nB) Kiwi\nC) Penguin\nD) Koala",
+                "4. Who was he first person to discover New Zealand?\nA) Captain James Cook\nB) Abel Tasman\nC) Abraham Lincoln\nD) Barack Obama",
+                "5. The Maori name for New Zealand means?\nA) Land Of Milk And Honey\nB) Australia's Brother\nC) Whale Rider\nD) Land of the long white cloud", };
+
+                string[] easyAnswers = { "A", "B", "B", "A", "D" };
+
+                Console.WriteLine("You have selected the easy difficulty. Let's get started!");
+
+
+
+
+            }
         }
-    }
 }
