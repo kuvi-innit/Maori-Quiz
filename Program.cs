ca0a39dbc8b946bbbdbdbc4cf90cef3481b11488
Kuvam Singh <ac121634@avcol.school.nz>
2023-05-16 11:55:03 +1200

    added methods to level selection AND changed == to .contains for string to be more secure

--- Program.cs
+++ Program.cs
@@ -7,10 +7,7 @@
             // Variables
             string name;
             string difficulty;
-            int scoreEasy = 0;
-            int scoreMedium = 0;
-            int scoreHard = 0;
-
+           
             // Introduction for the quiz
             Console.WriteLine("Welcome to the Moari Quiz!");
             Console.Write("Please enter your name: ");
@@ -27,132 +24,85 @@
                 difficulty = Console.ReadLine().ToLower();
             }
             Console.Clear();
-            // Easy Difficulty
+            Play(difficulty);
+        }
+
+        public static void Play(string difficulty)
+        {
+            int score = 0;
+
+            string[] questions = { };
+            string[] answers = { };
+
             if (difficulty == "1")
             {
-                string[] easyQuestions = {
+                //easy difficulty questions in array
+                questions = new string[] {
                 "1. What is the Maori term for 'New Zealand'?\nA) Aotearoa\nB) Hawaii\nC) Australia\nD) Fiji",
                 "2. What is the traditional Maori greeting?\nA) Aloha\nB) Kia Ora\nC) Hola\nD) Bonjour",
                 "3. What animal is a symbol of New Zealand?\nA) Emu\nB) Kiwi\nC) Penguin\nD) Koala",
                 "4. Who was he first person to discover New Zealand?\nA) Captain James Cook\nB) Abel Tasman\nC) Abraham Lincoln\nD) Barack Obama",
-                "5. The Maori name for New Zealand means?\nA) Land Of Milk And Honey\nB) Australia's Brother\nC) Whale Rider\nD) Land of the long white cloud", };
-
-                string[] easyAnswers = { "A", "B", "B", "A", "D" };
+                "5. The Maori name for New Zealand means?\nA) Land Of Milk And Honey\nB) Australia's Brother\nC) Whale Rider\nD) Land of the long white cloud"};
+                //easy difficulty questions (answers) in multi choice
+                answers = new string[] { "A", "B", "B", "A", "D" };
 
                 Console.WriteLine("You have selected the easy difficulty. Let's get started!");
-
-                // Easy Questions
-                for (int i = 0; i < easyQuestions.Length; i++)
-                {
-                    Console.WriteLine(easyQuestions[i]);
-                    string answerEasy = Console.ReadLine().ToUpper();
-
-                    // Easy Answer Validation
-                    while (answerEasy != "A" && answerEasy != "B" && answerEasy != "C" && answerEasy != "D")
-                    {
-                        Console.WriteLine("Please select a valid answer: A, B, C or D");
-                        answerEasy = Console.ReadLine().ToUpper();
-                    }
-
-                    // Easy Score
-                    if (answerEasy == easyAnswers[i])
-                    {
-                        Console.WriteLine("Correct"); scoreEasy++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Incorrect");
-                    }
-                }
-                Console.Clear();
-                // Results
-                Console.WriteLine("You have finished the quiz! Your score is: {0}/5", scoreEasy);
             }
-            // Hard Difficulty
             else if (difficulty == "2")
             {
-                // array of medium questions and answers
-                string[] mediumQuestions = {
+                //medium difficulty questions in array
+                questions = new string[] {
                "1. What does thank you mean in Moari?\nA) Nga Mihi\nB) Kia Ora\nC) Mihi Aroha\nD) Talofa",
                "2. What is the name of the famous Maori war dance?\nA) Haka\nB) Khalsa\nC) Ta tona\nD) Salsa ",
                "3. What is the traditional method of cooking in Maori culture?\nA) Khanah\nB) Hangi\nC) Apa Tihi\nD) Oni Ma",
                "4. Who were the first people to settle in New Zealand?\nA) The Maori\nB) The Indian\nC) European\nD) Samoan",
-               "5. What is the meaning of the word whanau in Maori?\nA) Friends\nB) Love\nC) Family\nD) Myself", };
-                string[] mediumAnswers = { "A", "A", "B", "A", "C" };
+               "5. What is the meaning of the word whanau in Maori?\nA) Friends\nB) Love\nC) Family\nD) Myself"};
+                //medium difficulty questions (answers) based of answers in multi choice
+                answers = new string[] { "A", "A", "B", "A", "C" };
 
                 Console.WriteLine("You have selected the medium difficulty. Let's get started!");
-
-                // medium Questions
-                for (int i = 0; i < mediumQuestions.Length; i++)
-                {
-                    Console.WriteLine(mediumQuestions[i]);
-                    string answermedium = Console.ReadLine().ToUpper();
-
-                    // medium Answer Validation
-                    while (answermedium != "A" && answermedium != "B" && answermedium != "C" && answermedium != "D")
-                    {
-                        Console.WriteLine("Please select a valid answer: A, B, C or D");
-                        answermedium = Console.ReadLine().ToUpper();
-                    }
-
-                    // medium Score
-                    if (answermedium == mediumAnswers[i])
-                    {
-                        Console.WriteLine("Correct"); scoreMedium++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Incorrect");
-                    }
-                }
-
-                // Results
-                Console.Clear();
-                Console.WriteLine("You have finished the quiz! Your score is: {0}/5", scoreMedium);
             }
-
-
-            // Hard Difficulty
             else if (difficulty == "3")
             {
-                // array of hard questions and answers
-                string[] hardQuestions = {
-                "1. What term is used to describe the traditional Maori form of art? \nA) Tuhituhi\nB) Whakairo\nC) Whakapapa\nD) Kowhaiwhai",
-                "2. What is the traditional Maori name for the South Island? \nA) Te Wai Pounamu\nB) Te Ika a Maui\nC) Te Aroha\nD) Te Manawa",
-                "3. What is the name of the paddle used in Maori ceremonial welcomes? \nA) Hoho Paddle\nB) Waipura Paddle\nC) Waka Paddle\nD) Tera tahi Paddle",
-                "4. What are the three baskets of knowledge in Maori culture?\nA) Basket of light, Basket of darkness, basket ball hoop\nB) Basket of Power, Basket of Beauty, Basket of Hope\nC) Basket of light, Basket of darkness, Basket of Pursuit \nD) Basket of flowers, Basket of smiles, Basket of positivity",
-                "5. What does the word “kia kaha” mean?\nA) Be weak\nB) Please come\nC) Thank you \nD) Stay strong ", };
-                string[] hardAnswers = { "B", "A", "A", "C", "D" };
+                //hard difficulty questions in array
+                questions = new string[] {
+                    "1. What term is used to describe the traditional Maori form of art? \nA) Tuhituhi\nB) Whakairo\nC) Whakapapa\nD) Kowhaiwhai",
+                    "2. What is the traditional Maori name for the South Island? \nA) Te Wai Pounamu\nB) Te Ika a Maui\nC) Te Aroha\nD) Te Manawa",
+                    "3. What is the name of the paddle used in Maori ceremonial welcomes? \nA) Hoho Paddle\nB) Waipura Paddle\nC) Waka Paddle\nD) Tera tahi Paddle",
+                    "4. What are the three baskets of knowledge in Maori culture?\nA) Basket of light, Basket of darkness, basket ball hoop\nB) Basket of Power, Basket of Beauty, Basket of Hope\nC) Basket of light, Basket of darkness, Basket of Pursuit \nD) Basket of flowers, Basket of smiles, Basket of positivity",
+                    "5. What does the word “kia kaha” mean?\nA) Be weak\nB) Please come\nC) Thank you \nD) Stay strong"};
+                //hard difficulty questions (answers) based of answers in multi choice
+                answers = new string[] { "B", "A", "A", "C", "D" };
 
                 Console.WriteLine("You have selected the hard difficulty. Let's get started!");
+            }
 
-                // Hard Questions
-                for (int i = 0; i < hardQuestions.Length; i++)
+            // Questions
+            for (int i = 0; i < questions.Length; i++)
+            {
+                Console.WriteLine(questions[i]);
+                string input = Console.ReadLine().ToUpper();
+
+                // Answer Validation
+                while (input != "A" && input != "B" && input != "C" && input != "D")
                 {
-                    Console.WriteLine(hardQuestions[i]);
-                    string answerHard = Console.ReadLine().ToUpper();
-
-                    // Hard Answer Validation
-                    while (answerHard != "A" && answerHard != "B" && answerHard != "C" && answerHard != "D")
-                    {
-                        Console.WriteLine("Please select a valid answer: A, B, C or D");
-                        answerHard = Console.ReadLine().ToUpper();
-                    }
-
-                    // Hard Score
-                    if (answerHard == hardAnswers[i])
-                    {
-                        Console.WriteLine("Correct"); scoreHard++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Incorrect");
-                    }
+                    Console.WriteLine("Please select a valid answer: A, B, C or D");
+                    input = Console.ReadLine().ToUpper();
                 }
 
-                // Results
-                Console.WriteLine("You have finished the Maori quiz! Your score is: {0}/5", scoreHard);
+                // Score
+                if (input.Contains(answers[i]))
+                {
+                    Console.WriteLine("Correct"); score++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect");
+                }
             }
+            Console.Clear();
+            // Results
+            Console.WriteLine($"You have finished the quiz! Your score is: {score}/5");
         }
     }
 }
