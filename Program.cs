7a0d5a1827205e923fab16b774b92fcadd5a7e39
kuvi:) <125934714+kuvi-innit@users.noreply.github.com>
2023-05-25 21:28:19 +1200

    Used methods, procedures that use parameters effectively

--- Program.cs
+++ Program.cs
@@ -4,76 +4,69 @@
     {
         static void Main(string[] args)
         {
-            // Variables
-            string name;
-            string difficulty;
-           
-            // Introduction for the quiz
-            Console.WriteLine("Welcome to the Moari Quiz!");
+           // Introduction for the quiz
+            Console.WriteLine("Welcome to the Maori Quiz!");
             Console.Write("Please enter your name: ");
-            name = Console.ReadLine();
+            string name = Console.ReadLine();
 
             // Difficulty Selection
-            Console.WriteLine("Hello," + name + "!"); Console.WriteLine("Which level would you like to play: 1) Easy 2) Medium 3) Hard");
-            difficulty = Console.ReadLine().ToLower();
+            string difficulty = GetDifficulty();
 
-            // Difficulty Selection Validation
-            while (difficulty != "1" && difficulty != "2" && difficulty != "3")
-            {
-                Console.WriteLine("Please select a valid difficulty: 1 or 2 or 3");
-                difficulty = Console.ReadLine().ToLower();
-            }
             Console.Clear();
             Play(difficulty);
+        }
+
+        public static string GetDifficulty()
+        {
+            string difficulty;
+            Console.WriteLine("Hello, {0}!", name);
+            Console.WriteLine("Which level would you like to play: 1) Easy 2) Medium 3) Hard");
+
+            do
+            {
+                difficulty = Console.ReadLine().ToLower();
+                if (difficulty != "1" && difficulty != "2" && difficulty != "3")
+                {
+                    Console.WriteLine("Please select a valid difficulty: 1, 2, or 3");
+                }
+            } while (difficulty != "1" && difficulty != "2" && difficulty != "3");
+
+            return difficulty;
         }
 
         public static void Play(string difficulty)
         {
             int score = 0;
 
-            string[] questions = { };
-            string[] answers = { };
+            string[] questions;
+            string[] answers;
 
             if (difficulty == "1")
             {
-                //easy difficulty questions in array
                 questions = new string[] {
-                "1. What is the Maori term for 'New Zealand'?\nA) Aotearoa\nB) Hawaii\nC) Australia\nD) Fiji",
-                "2. What is the traditional Maori greeting?\nA) Aloha\nB) Kia Ora\nC) Hola\nD) Bonjour",
-                "3. What animal is a symbol of New Zealand?\nA) Emu\nB) Kiwi\nC) Penguin\nD) Koala",
-                "4. Who was he first person to discover New Zealand?\nA) Captain James Cook\nB) Abel Tasman\nC) Abraham Lincoln\nD) Barack Obama",
-                "5. The Maori name for New Zealand means?\nA) Land Of Milk And Honey\nB) Australia's Brother\nC) Whale Rider\nD) Land of the long white cloud"};
-                //easy difficulty questions (answers) in multi choice
+                    "1. What is the Maori term for 'New Zealand'?\nA) Aotearoa\nB) Hawaii\nC) Australia\nD) Fiji",
+                    // ...
+                };
                 answers = new string[] { "A", "B", "B", "A", "D" };
 
                 Console.WriteLine("You have selected the easy difficulty. Let's get started!");
             }
             else if (difficulty == "2")
             {
-                //medium difficulty questions in array
                 questions = new string[] {
-               "1. What does thank you mean in Moari?\nA) Nga Mihi\nB) Kia Ora\nC) Mihi Aroha\nD) Talofa",
-               "2. What is the name of the famous Maori war dance?\nA) Haka\nB) Khalsa\nC) Ta tona\nD) Salsa ",
-               "3. What is the traditional method of cooking in Maori culture?\nA) Khanah\nB) Hangi\nC) Apa Tihi\nD) Oni Ma",
-               "4. Who were the first people to settle in New Zealand?\nA) The Maori\nB) The Indian\nC) European\nD) Samoan",
-               "5. What is the meaning of the word whanau in Maori?\nA) Friends\nB) Love\nC) Family\nD) Myself"};
-                //medium difficulty questions (answers) based of answers in multi choice
+                    "1. What does thank you mean in Maori?\nA) Nga Mihi\nB) Kia Ora\nC) Mihi Aroha\nD) Talofa",
+                    // ...
+                };
                 answers = new string[] { "A", "A", "B", "A", "C" };
 
                 Console.WriteLine("You have selected the medium difficulty. Let's get started!");
             }
-            else if (difficulty == "3")
+            else
             {
-                //hard difficulty questions in array
                 questions = new string[] {
-                    "1. What term is used to describe the traditional Maori form of art? \nA) Tuhituhi\nB) Whakairo\nC) Whakapapa\nD) Kowhaiwhai",
-                    "2. What is the traditional Maori name for the South Island? \nA) Te Wai Pounamu\nB) Te Ika a Maui\nC) Te Aroha\nD) Te Manawa",
-                    "3. What is the name of the paddle used in Maori ceremonial welcomes? \nA) Hoho Paddle\nB) Waipura Paddle\nC) Waka Paddle\nD) Tera tahi Paddle",
-                    "4. What are the three baskets of knowledge in Maori culture?\nA) Basket of light, Basket of darkness, basket ball hoop
-                    \nB) Basket of Power, Basket of Beauty, Basket of Hope\nC) Basket of light, Basket of darkness, Basket of Pursuit 
-                    \nD) Basket of flowers, Basket of smiles, Basket of positivity",
-                    "5. What does the word “kia kaha” mean?\nA) Be weak\nB) Please come\nC) Thank you \nD) Stay strong"};
-                //hard difficulty questions (answers) based of answers in multi choice
+                    "1. What term is used to describe the traditional Maori form of art?\nA) Tuhituhi\nB) Whakairo\nC) Whakapapa\nD) Kowhaiwhai",
+                    // ...
+                };
                 answers = new string[] { "B", "A", "A", "C", "D" };
 
                 Console.WriteLine("You have selected the hard difficulty. Let's get started!");
@@ -83,28 +76,38 @@
             for (int i = 0; i < questions.Length; i++)
             {
                 Console.WriteLine(questions[i]);
-                string input = Console.ReadLine().ToUpper();
-
-                // Answer Validation
-                while (input != "A" && input != "B" && input != "C" && input != "D")
-                {
-                    Console.WriteLine("Please select a valid answer: A, B, C or D");
-                    input = Console.ReadLine().ToUpper();
-                }
+                string input = GetValidAnswer();
 
                 // Score
                 if (input.Contains(answers[i]))
                 {
-                    Console.WriteLine("Correct"); score++;
+                    Console.WriteLine("Correct");
+                    score++;
                 }
                 else
                 {
                     Console.WriteLine("Incorrect");
                 }
             }
+
             Console.Clear();
             // Results
-            Console.WriteLine($"You have finished the quiz! Your score is: {score}/5");
+            Console.WriteLine($"You have finished the quiz! Your score is: {score}/{questions.Length}");
+        }
+
+        public static string GetValidAnswer()
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine().ToUpper();
+                if (input != "A" && input != "B" && input != "C" && input != "D")
+                {
+                    Console.WriteLine("Please select a valid answer: A, B, C, or D");
+                }
+            } while (input != "A" && input != "B" && input != "C" && input != "D");
+
+            return input;
         }
     }
 }
